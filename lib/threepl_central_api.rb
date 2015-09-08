ea62ac6dd12f1e3694fca10f6f8b32eae9eaa7f4
Dave Nunez <dnunez24@gmail.com>
2015-09-07 21:22:27 -0700

    Remove References to Old Response Class

--- lib/threepl_central_api.rb
+++ lib/threepl_central_api.rb
@@ -1,7 +1,6 @@
 module ThreePLCentralAPI
   autoload :SOAPClient, 'threepl_central_api/soap_client'
+  autoload :Error, 'threepl_central_api/error'
   autoload :SmallParcelOrdersRequest, 'threepl_central_api/small_parcel_orders_request'
   autoload :SmallParcelOrdersResponse, 'threepl_central_api/small_parcel_orders_response'
-  autoload :Response, 'threepl_central_api/response'
-  autoload :Error, 'threepl_central_api/error'
 end
